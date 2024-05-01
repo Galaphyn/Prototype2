63b6b872205ac2c7d470181a6ddafc81a4bc9d32
Galaphyn <96713558+Galaphyn@users.noreply.github.com>
2024-04-30 18:45:13 -0700

    Add files via upload

--- Assets/Scripts/IndependantSpawner.cs
+++ Assets/Scripts/IndependantSpawner.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class IndependantSpawner : MonoBehaviour
 {
+    public GameObject enemyType;
+    private GameObject newEnemy;
+    public float spawnCooldown = 5;
+    private float timer;
+    public int maxEnemy = 4;
+    private int numEnemy;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        sr.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (numEnemy < maxEnemy && Time.time > timer)
+        {
+            newEnemy = Instantiate(enemyType);
+            numEnemy++;
+            newEnemy.transform.position = transform.position;
+            timer = Time.time + spawnCooldown;
+        }
     }
 }
